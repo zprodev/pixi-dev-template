53e0493f9c1c9a7c80376de3251fb1f677590ab3
zprodev <zproject.develop@gmail.com>
2017-10-19 02:22:14 +0900

    Add assets livereload support

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const SCREEN_HEIGHT = 500;
 let app = new PIXI.Application(SCREEN_WIDTH, SCREEN_HEIGHT, {backgroundColor : 0x1099bb});
 document.body.appendChild(app.view);
 
-let knight = PIXI.Sprite.fromImage('images/knight.png')
+let knight = PIXI.Sprite.fromImage('img/knight.png')
 knight.anchor.set(0.5);
 knight.x = SCREEN_WIDTH / 2;
 knight.y = SCREEN_HEIGHT / 2;
