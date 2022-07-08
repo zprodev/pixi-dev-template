96657cb05ba8268f583a2e714a16dd2aa954520c
zprodev <zproject.develop@gmail.com>
2022-07-08 15:10:46 +0900

    support pixi6

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {Application, Sprite} from 'pixi.js';
 const SCREEN_WIDTH = 500;
 const SCREEN_HEIGHT = 500;
 
-let app = new Application(SCREEN_WIDTH, SCREEN_HEIGHT, {backgroundColor : 0x1099bb});
+let app = new Application({width: SCREEN_WIDTH, height:SCREEN_HEIGHT, backgroundColor : 0x1099bb});
 document.body.appendChild(app.view);
 
 let knight = Sprite.from('img/knight.png')
